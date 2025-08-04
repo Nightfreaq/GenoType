3cd362dafe8efbd4e603eb774b706acdef68217b
Michał~ <mi.kielbik@gmail.com>
2025-08-04 19:16:38 +0200

    header js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ document.addEventListener('click', () => {
     nav.classList.remove('show')
 }
 )
+
+function rotate (){
+
+const arrow = document.querySelector('.fa-arrow-down')
+
+if(window.scrollY >= 300){
+  arrow.style.rotate = '180deg'
+  arrow.classList.remove('bounce-top')
+} else {
+  arrow.style.rotate = '0deg'
+  arrow.classList.add('bounce-top')
+}
+}
+
+window.addEventListener('scroll', rotate)
